4dc02bb489de2206dded3ea14817e8b5983c3a82
Seif AbdelAziz <seif_abdelaziz@outlook.com>
2023-06-25 18:59:07 +0300

    Products Repo V1

---
+++ E_Commerce.DAL/Repositiories/Products/IProductsRepo.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.DAL;
+
+public interface IProductsRepo : IGenericRepo<Product>
+{
+    Product? GetProductDetails(Guid id);
+    Product? GetProductReviews(Guid id);
+    Product? GetProductCategories(Guid id);
+
+}
---
+++ E_Commerce.DAL/Repositiories/Products/ProductsRepo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.DAL;
+
+public class ProductsRepo : GenericRepo<Product>, IProductsRepo
+{
+    private readonly E_CommerceContext _context;
+
+    public ProductsRepo(E_CommerceContext context) : base(context)
+    {
+        _context = context;
+    }
+
+
+    public Product? GetProductDetails(Guid id)
+    {
+        return _context.Set<Product>()
+            .Include(p => p.ProductImages)
+            .Include(p => p.Product_Color_Size_Quantity)
+            .FirstOrDefault(p => p.Id == id);
+    }
+
+    public Product? GetProductReviews(Guid id)
+    {
+        return _context.Set<Product>()
+            .Include(p => p.Reviews)
+            .FirstOrDefault(p => p.Id == id);
+    }
+    public Product? GetProductCategories(Guid id)
+    {
+        return _context.Set<Product>()
+            .Include(p => p.Categories)
+            .FirstOrDefault(p => p.Id == id);
+    }
+}
