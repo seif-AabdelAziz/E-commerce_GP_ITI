d8e1e56f2d8824d18bb44da845d24c830c4bff96
Mahmoud Elish <mahmoudelish99@gmail.com>
2023-06-25 19:24:12 +0300

    OrderRepo

---
+++ E_Commerce.DAL/Repositiories/orders/IOrderRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL
+{
+    public interface IOrderRepo :IGenericRepo<Order>
+    {
+        Customer? GetCustomerByOrderId(Guid id);
+        List<OrderProduct> GetOrderProducts(Guid id);
+        Order? GetOrderProductsAndCustomer(Guid id);
+        List<OrderProduct> GetOrderDetails(Guid id);
+    }
+}
---
+++ E_Commerce.DAL/Repositiories/orders/OrderRepo.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL
+{
+    public class OrderRepo : GenericRepo<Order>, IOrderRepo
+    {
+        private readonly E_CommerceContext _context;
+        public OrderRepo(E_CommerceContext context) : base(context)
+        {
+            _context=context;
+        }
+
+        public Customer? GetCustomerByOrderId(Guid id)
+        {
+            return _context.Set<Order>().Where(o => o.Id == id).Select(o => o.Customer).FirstOrDefault();
+        }
+
+        public List<OrderProduct> GetOrderDetails(Guid id)
+        {
+            return _context.Set<OrderProduct>().Include(op => op.Product).Where(op => op.OrderId == id).ToList();
+        }
+
+        public List<OrderProduct> GetOrderProducts(Guid id)
+        {
+            return _context.Set<OrderProduct>().Where(op => op.OrderId == id).ToList();
+        }
+
+        public Order? GetOrderProductsAndCustomer(Guid id)
+        {
+            return _context.Set<Order>().Include(o => o.OrderProducts).ThenInclude(op => op.Product)
+                                        .Include(o => o.Customer).Where(o => o.Id == id)
+                                        .FirstOrDefault();
+        }   
+    }
+}
