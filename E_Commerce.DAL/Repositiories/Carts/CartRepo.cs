99b33819d7e52cf16483c0100e4c437f5ceec1c0
Ethar  Abdelrahman <etharelbosily@gmail.com>
2023-06-25 19:07:28 +0300

    CartRep

---
+++ E_Commerce.DAL/Repositiories/Carts/CartRepo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL
+{
+    public class CartRepo : GenericRepo<Cart>, ICartRepo
+    {
+        private readonly E_CommerceContext _context;
+    
+        public CartRepo(E_CommerceContext context) : base(context)
+        {
+            _context = context;
+        }
+   
+        public IEnumerable<Cart> GetCartProductByCustomerId(Guid CustomerId)
+        {
+            //Guid id = new Guid(CustomerId);
+
+            return _context.Carts
+            .Include(c => c.Products)
+             .Where(c => c.CustomerId == CustomerId)
+             .ToList();
+        }
+    }
+}
--- E_Commerce.DAL/UnitOfWork/UnitOfWork.cs
+++ E_Commerce.DAL/UnitOfWork/UnitOfWork.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 namespace E_Commerce.DAL
 {
     public class UnitOfWork : IUnitOfWork
+
     {
+        public ICartRepo CartRepo { get; set; }
         private readonly E_CommerceContext _context;
 
-        public UnitOfWork(E_CommerceContext context)
+        public UnitOfWork(E_CommerceContext context , ICartRepo cartRepo)
         {
             _context = context;
+            CartRepo = cartRepo;
         }
         public int SaveChange()
         {
