c2fe2db4b2a5606e2c5235d86377e174991258fb
Abdelrahman Emad Farouk <50057152+AbdelrahmanEmad98@users.noreply.github.com>
2023-06-27 00:07:03 +0300

    v12

--- E_Commerce/Program.cs
+++ E_Commerce/Program.cs
@@ -42,11 +42,7 @@ builder.Services.AddScoped<IProductManager, ProductManager>();
 #endregion
 
 
-<<<<<<< Updated upstream
-=======
 
-
->>>>>>> Stashed changes
 #region Identity
 
 //Mainly specify the context and the type of the user that the UserManger will use
---
+++ E_Commrce.BL/Dto/Customer/CustomerUpdateDto.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.DAL;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.BL
+{
+    public class CustomerUpdateDto
+    {
+        public Guid Id { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string MidName { get; set; } = string.Empty;
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string LastName { get; set; } = string.Empty;
+
+        public string Street { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public Countries Country { get; set; }
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
