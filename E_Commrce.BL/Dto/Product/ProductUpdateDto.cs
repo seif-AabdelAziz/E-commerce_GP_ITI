62e645bbec0b58eb20db5d01105b5a0c5459cc90
Seif AbdelAziz <seif_abdelaziz@outlook.com>
2023-06-27 18:01:47 +0300

    Product Manager V5

---
+++ E_Commrce.BL/Dto/Product/ProductUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.BL;
+
+public class ProductUpdateDto
+{
+    public Guid Id { get; set; }
+    [MinLength(3)]
+    [MaxLength(30)]
+    public string Name { get; set; } = null!;
+    [MaxLength(500)]
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    [Range(0, 1)]
+    public decimal Discount { get; set; }
+    [Range(0, 5)]
+    public decimal Rate { get; set; }
+    public List<ProductImageDto> ProductImages { get; set; } = null!;
+    public List<ProductInfoDto> ProductInfo { get; set; } = null!;
+    public List<ProductAddCategoryDto> ProductCategories { get; set; } = null!;
+}
