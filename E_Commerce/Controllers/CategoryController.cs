a31623d7f330527116e003ac3cf818a0a8da0c3c
Mahmoud Khaled <mahmoudxkhaled@gmail.com>
2023-07-02 13:10:01 +0300

    push v2

---
+++ E_Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,135 @@
+﻿using E_Commerce.BL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : ControllerBase
+{
+
+    #region CTOR
+
+    private readonly ICategoriesManager _categoryManager;
+
+    public CategoryController(ICategoriesManager categoryManager)
+    {
+        _categoryManager = categoryManager;
+    }
+    #endregion
+
+    #region All Categories
+
+    [HttpGet]
+    public ActionResult<List<CategoryReadDto>> AllCategories()
+    {
+        List<CategoryReadDto> categories = _categoryManager.GetAllCategories();
+        return categories;
+    }
+    #endregion
+
+    #region Add Category
+
+
+    [HttpPost("Add")]
+    public ActionResult AddCategory(CategoryAddDto category)
+    {
+
+        bool request = _categoryManager.AddCategory(category);
+
+        if (request == false)
+        {
+            return BadRequest();
+        }
+        return Ok("category Added Successfully");
+
+    }
+    #endregion
+
+    #region Delete Category
+
+    [HttpDelete("Delete/{id}")]
+    public ActionResult DeleteCategory(Guid id)
+    {
+
+        bool request = _categoryManager.DeleteCategory(id);
+        if (request == false)
+        {
+            return BadRequest();
+        }
+        return Ok("Category Deleted Successfully");
+
+    }
+
+
+    #endregion
+
+    #region Update Category
+
+    [HttpGet("Update/{id}")]
+    public ActionResult<CategoryUpdateDto> CategoryToUpdate(Guid id)
+    {
+
+        CategoryUpdateDto? category = _categoryManager.CategoryToUpdate(id);
+        if (category == null)
+        {
+
+            return BadRequest();
+        }
+        return category;
+
+    }
+
+    [HttpPut("Update")]
+    public ActionResult UpdateCategory(CategoryUpdateDto categoryUpdateDto)
+    {
+
+        bool request = _categoryManager.UpdateCategory(categoryUpdateDto);
+        if (request == false)
+        {
+            return BadRequest();
+        }
+        return Ok("Category Updated Successfully");
+
+    }
+
+    #endregion
+
+    #region Get Category By Id
+
+    [HttpGet]
+    [Route("Details/{id}")]
+    public ActionResult<CategoryReadDto> CategoryDetail(Guid id)
+    {
+
+        CategoryReadDto? categoryDetails = _categoryManager.GetCategoryById(id);
+        if (categoryDetails == null)
+        {
+            return BadRequest();
+        }
+
+        return categoryDetails;
+    }
+
+    #endregion
+
+    #region Get subcategories for Parent Category
+
+    [HttpGet]
+    [Route("subCategories/{id}")]
+    public ActionResult<List<SubCategoryReadDto>> GetSubCategories(Guid id)
+    {
+        List<SubCategoryReadDto> subCategories = _categoryManager.GetSubCategories(id).ToList();
+
+        if (subCategories == null)
+        {
+            return BadRequest();
+        }
+
+
+        return subCategories;
+    }
+    #endregion
+
+
+}
---
+++ E_Commrce.BL/dto/user/UserUpdateDto.cs
@@ -0,0 +1,51 @@
+﻿using E_Commerce.DAL;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.BL;
+
+public class UserUpdateDto
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "First Name is required")]
+    [MinLength(3, ErrorMessage = "Name can't be less that three characters")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Middle Name is required")]
+    public string MidName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Last Name is required")]
+    [MinLength(3, ErrorMessage = "Name can't be less that three characters")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    [RegularExpression(@"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email format is not valid it must be in the form name@example.com")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password is required")]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one letter and one digit.")]
+    public string Password { get; set; } = string.Empty;
+
+    [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Invalid phone number.")]
+    [Required(ErrorMessage = "PhoneNumber is required")]
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Street is required")]
+    public string Street { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "City is required")]
+    public string City { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Country is required")]
+    public Countries Country { get; set; }
+
+    [Required(ErrorMessage = "City is required")]
+    [StringLength(2, ErrorMessage = "Name on card must be at least 2 characters long.")]
+    public string? NameOnCard { get; set; } = string.Empty;
+    [Required(ErrorMessage = "City is required")]
+    public decimal? CardNumber { get; set; }
+    [Required(ErrorMessage = "City is required")]
+    public DateTime? ExpireDate { get; set; }
+
+
+}
