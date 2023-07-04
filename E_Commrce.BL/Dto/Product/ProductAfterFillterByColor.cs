8b3cde2e378aee48baecac3a2a6712e327dc2363
Abdelrahman Emad Farouk <50057152+AbdelrahmanEmad98@users.noreply.github.com>
2023-07-04 21:42:37 +0300

    v00vd

---
+++ E_Commrce.BL/Dto/Product/ProductAfterFillterByColor.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BL
+{
+    public class ProductAfterFillterByColor
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Rate { get; set; }
+        public List<ProductImageDto> ProductImages { get; set; } = new List<ProductImageDto>();
+        public List<ProductInfoDto> ProductInfo { get; set; } = new List<ProductInfoDto>();
+        
+    }
+}
---
+++ E_Commrce.BL/Dto/Product/ProductFillterByColor.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BL
+{
+    public class ProductFillterByColor
+    {
+        public Guid Id { get; set; }
+        public Color Color { get; set; }
+
+    }
+}
