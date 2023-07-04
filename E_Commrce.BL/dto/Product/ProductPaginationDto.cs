4b6946739f3ae9d03015a73f63d889b793c2d274
Mahmoud Khaled <mahmoudxkhaled@gmail.com>
2023-07-04 21:15:33 +0300

    p

---
+++ E_Commrce.BL/dto/Product/ProductPaginationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BL;
+
+public class ProductPaginationDto
+{
+
+    public List<ProductDetailsReadDto> items { get; set; }= new List<ProductDetailsReadDto>();
+    public int totalCount { get; set; }
+}
