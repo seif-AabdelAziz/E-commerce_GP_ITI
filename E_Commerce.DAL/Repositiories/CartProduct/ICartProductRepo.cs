ab466a4cb008cc0de08f64f2ae8698a37ec5a6ef
Mahmoud-El-naggar <melnaggar.1399@gmail.com>
2023-07-05 15:32:31 +0300

    CheckOut Handling

--- E_Commerce.DAL/Repositiories/CartProduct/ICartProductRepo.cs
+++ E_Commerce.DAL/Repositiories/CartProduct/ICartProductRepo.cs
@@ -4,12 +4,7 @@ namespace E_Commerce.DAL
      public interface ICartProductRepo : IGenericRepo<CartProduct> 
     {
         CartProduct? GetById(Guid productId,Guid cartId);
-<<<<<<< Updated upstream
         CartProduct? GetCartProductByCustomerId(Guid CustomerId);
 
-
-=======
-        
->>>>>>> Stashed changes
     }
 }
