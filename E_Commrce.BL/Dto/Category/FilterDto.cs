bc2b58cee8568f50b2c38650cf763149af87cb69
Abdelrahman Emad Farouk <50057152+AbdelrahmanEmad98@users.noreply.github.com>
2023-07-06 19:24:10 +0300

    v03

---
+++ E_Commrce.BL/Dto/Category/FilterDto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BL
+{
+    public class FilterDto
+    {
+        public List<Guid>? ParentId { get; set; }
+        public List<Guid>?  ChildId { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public List<string>? Color { get; set; }   
+        public List<decimal>? Rate { get; set; }
+        public List<string>? Size { get; set; }
+    }
+}
