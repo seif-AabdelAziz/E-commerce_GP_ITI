19a10567427aa9e41509ac4791c0611d5d3f3a3f
Ethar  Abdelrahman <etharelbosily@gmail.com>
2023-07-08 12:55:21 +0300

    vv

---
+++ E_Commrce.BL/Dto/order/OrderTableDto.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce.BL
+{
+    public class OrderTableDto
+    {
+        public Guid OrderId { get; set; }
+        public string PaymentStatus { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
